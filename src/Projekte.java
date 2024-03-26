fb3d24ee2979df015f2dbc74cb558837b8319e1b
hannan <hannanalibutt10@gmail.com>
2024-03-26 15:57:08 +0100

    Klasse "Projekte" implementiert

--- src/Projekte.java
+++ src/Projekte.java
@@ -1,2 +1,45 @@
+import java.time.LocalDate;
+
 public class Projekte {
+    private String name;
+    private LocalDate erstellungsdatum;
+    private LocalDate deadline;
+
+    /**
+     * Konstruktor der Klasse "Projekte" mit dem Inhalt: Name, Deadline und Erstellungsdatum
+     *
+     * Getter und Setter Methoden sind auch da ._.
+     *
+     * @param name
+     * @param deadline
+     */
+    public Projekte(String name, String deadline){
+        this.name = name;
+        this.erstellungsdatum = LocalDate.now();
+        this.deadline = LocalDate.parse(deadline);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getErstellungsdatum() {
+        return erstellungsdatum;
+    }
+
+    public void setErstellungsdatum(LocalDate erstellungsdatum) {
+        this.erstellungsdatum = erstellungsdatum;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        String pDeadline = deadline;
+    }
 }
