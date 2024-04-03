2a74405386eeaf78ace5cff91800ec02fdbc6956
hannan <hannanalibutt10@gmail.com>
2024-04-03 14:26:09 +0200

    Deklarierung der Beziehungsattribute

--- src/AufgabenUndProjektverwaltungstool.java
+++ src/AufgabenUndProjektverwaltungstool.java
@@ -1,2 +1,7 @@
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class AufgabenUndProjektverwaltungstool {
+    private PriorityQueue<Aufgaben> AufgabenPriorityQueue;
+    private List<Projekte>  ProjektListe;
 }
