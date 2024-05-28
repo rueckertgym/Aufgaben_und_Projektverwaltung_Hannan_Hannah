c73f2e5acf63ae178e10a192f10e7f17fb8fb9f3
buttha <hannanalibutt10@gmail.com>
2024-05-28 08:19:09 +0200

    Test Klasse

--- src/AufgabenUndProjektverwaltungstool.java
+++ src/AufgabenUndProjektverwaltungstool.java
@@ -16,7 +16,7 @@ public class AufgabenUndProjektverwaltungstool {
 
 
     public AufgabenUndProjektverwaltungstool() {
-        PriorityQueue<Aufgaben> AufgabenPriorityQueue = new PriorityQueue<>(Comparator.comparing(Aufgaben::getWichtig));
+        AufgabenPriorityQueue = new PriorityQueue<>(Comparator.comparing(Aufgaben::getWichtig));
         ProjektListe = new List<>();
     }
 
